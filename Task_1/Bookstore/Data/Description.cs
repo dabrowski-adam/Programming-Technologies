7ae3a99093736d87807cb361a4204b185b1d4a2c
Adam Dąbrowski <adam.dabrowski@htdevelopers.com>
2018-11-04 22:04:57 +0100

    Split Book into Description and price

---
+++ Task_1/Bookstore/Data/Description.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Data
+{
+    public class Description
+    {
+        public String Title { get; private set; }
+        public String Author { get; private set; }
+
+        public Description(String title, String author)
+        {
+            this.Title = title;
+            this.Author = author;
+        }
+    }
+}
