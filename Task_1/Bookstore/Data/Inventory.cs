5994fae7c0276fe364893838c321036683da32c9
Adam Dąbrowski <adam.dabrowski@htdevelopers.com>
2018-11-04 22:56:08 +0100

    Implement new Inventory behaviour

--- Task_1/Bookstore/Data/Inventory.cs
+++ Task_1/Bookstore/Data/Inventory.cs
@@ -1,29 +1,62 @@
-﻿using System.Collections.Generic;
+﻿using System.Collections;
+using System.Collections.Generic;
 
 namespace Data
 {
-
-    public class Inventory : Dictionary<ISBN, int>
-
+    public class Inventory : IDictionary<ISBN, int>
     {
-        internal new virtual void Add(ISBN key, int value)
-        {
-            if (!base.ContainsKey(key))
-            {
-                base.Add(key, value);
-            }
-            else;
-        }
-        /*
-                internal new virtual void Add(ISBN key)
-                 {
-                      if (!base.ContainsKey(key))
-                    {
-                        base.Add(key);
-                    }
-                 }
+        private Dictionary<ISBN, int> data;
 
+        public Inventory() {
+            this.data = new Dictionary<ISBN, int>();
+        }
+
+        public int this[ISBN key] { get => this.data[key]; set { this.data[key] = value; } }
+
+        public ICollection<ISBN> Keys => this.data.Keys;
+
+        public ICollection<int> Values => this.data.Values;
+
+        public int Count => this.data.Count;
+
+        public bool IsReadOnly => throw new System.NotImplementedException();
+
+        public void Add(ISBN key, int value)
+        {
+            if (this.data.ContainsKey(key)) {
+                this.data[key] = this.data[key] + value;
+            } else {
+                this.data.Add(key, value);
             }
-            */
+        }
+
+        public void Add(KeyValuePair<ISBN, int> item)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Clear()
+        {
+            this.data.Clear();
+        }
+
+        public bool Contains(KeyValuePair<ISBN, int> item) => throw new System.NotImplementedException();
+
+        public bool ContainsKey(ISBN key) => this.data.ContainsKey(key);
+
+        public void CopyTo(KeyValuePair<ISBN, int>[] array, int arrayIndex)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public IEnumerator<KeyValuePair<ISBN, int>> GetEnumerator() => throw new System.NotImplementedException();
+
+        public bool Remove(ISBN key) => this.data.Remove(key);
+
+        public bool Remove(KeyValuePair<ISBN, int> item) => throw new System.NotImplementedException();
+
+        public bool TryGetValue(ISBN key, out int value) => this.data.TryGetValue(key, out value);
+
+        IEnumerator IEnumerable.GetEnumerator() => this.data.GetEnumerator();
     }
 }
--- Task_1/Bookstore/DataTests2/InventoryTestData.cs
+++ Task_1/Bookstore/DataTests2/InventoryTestData.cs
@@ -1,9 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using Xunit;
-using Data;
+﻿using System.Collections.Generic;
 using System.Collections;
+using Data;
 
 namespace DataTests
 {
