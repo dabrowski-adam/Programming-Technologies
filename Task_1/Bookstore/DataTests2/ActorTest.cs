5846c7542c720814451280fca68ab28075b153dd
Adam Dąbrowski <adam.dabrowski@htdevelopers.com>
2018-11-04 19:28:29 +0100

    Add tests for Actor

---
+++ Task_1/Bookstore/DataTests2/ActorTest.cs
@@ -0,0 +1,21 @@
+﻿using Xunit;
+using Data;
+
+namespace DataTests
+{
+    public class ActorTest
+    {
+        #region NameTest
+        [Theory]
+        [InlineData("John Smith")]
+        [InlineData("Maria Skłodowska-Curie")]
+        [InlineData("Los Nombres Españoles Son Demasiado Largos")]
+        public void NameTest(string name)
+        {
+            Actor actor = new Actor(name);
+            Assert.Equal(name, actor.Name);
+        }
+        #endregion
+
+    }
+}
