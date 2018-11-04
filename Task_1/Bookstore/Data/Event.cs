3f7b72426fc38d059e4a42206aaf0eb4200c21f0
Adam Dąbrowski <adam.dabrowski@htdevelopers.com>
2018-11-04 19:12:38 +0100

    Fix Event.cs

--- Task_1/Bookstore/Data/Event.cs
+++ Task_1/Bookstore/Data/Event.cs
@@ -5,11 +5,11 @@ namespace Data
 {
     public class Event
     {
-        private List<Invoice> Invoices { get; private set; }
+        public List<Invoice> Invoices { get; private set; }
 
         public Event(List<Invoice> invoices)
         {
-            this.Invoices = new List<Invoice>(invoices)
+            this.Invoices = new List<Invoice>(invoices);
         }
     }
 }
