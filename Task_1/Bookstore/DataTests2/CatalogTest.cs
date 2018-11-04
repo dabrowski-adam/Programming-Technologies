02ac56029a81206821860d93cdbef144404ca04a
Adam Dąbrowski <adam.dabrowski@htdevelopers.com>
2018-11-04 20:43:56 +0100

    Add basic Catalog tests

---
+++ Task_1/Bookstore/DataTests2/CatalogTest.cs
@@ -0,0 +1,43 @@
+﻿using Xunit;
+using Data;
+
+namespace DataTests
+{
+    public class CatalogTest
+    {
+        #region CatalogConstructor
+        [Fact]
+        public void CatalogConstructor()
+        {
+            Catalog catalog = new Catalog();
+            Assert.Empty(catalog);
+        }
+        #endregion
+
+        #region CatalogInsert
+        [Theory]
+        [ClassData(typeof(BookTestData))]
+        public void CatalogInsert(Book book)
+        {
+            Catalog catalog = new Catalog();
+            ISBN isbn = new ISBN("0123456789123");
+            catalog.Add(isbn, book);
+            Assert.Single(catalog);
+            Assert.Equal(book, catalog[isbn]);
+        }
+        #endregion
+
+        #region CatalogRemove
+        [Theory]
+        [ClassData(typeof(BookTestData))]
+        public void CatalogRemove(Book book)
+        {
+            Catalog catalog = new Catalog();
+            ISBN isbn = new ISBN("0123456789123");
+            catalog.Add(isbn, book);
+            catalog.Remove(isbn);
+            Assert.Empty(catalog);
+        }
+        #endregion
+    }
+}
---
+++ Task_1/Bookstore/DataTests2/CatalogTestData.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Data;
+
+namespace DataTests
+{
+    public class CatalogTestData : IEnumerable<object[]>
+    {
+        public IEnumerator<object[]> GetEnumerator()
+        {
+            yield return new object[] { new Book("The Final Empire", "Brandon Sanderson") };
+            yield return new object[] { new Book("Ostatnie Życzenie", "Andrzej Sapkowski") };
+            yield return new object[] { new Book("Harry Potter and the Philosopher's Stone", "J.K. Rowling") ;
+        }
+
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+    }
+}
