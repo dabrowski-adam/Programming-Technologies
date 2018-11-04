19c7fe631697849954ffcfeac63b2a8c7a6f5f36
Adam Dąbrowski <adam.dabrowski@htdevelopers.com>
2018-11-04 22:09:16 +0100

    Fix failed merge

--- Task_1/Bookstore/Data/Inventory.cs
+++ Task_1/Bookstore/Data/Inventory.cs
@@ -2,11 +2,7 @@
 
 namespace Data
 {
-<<<<<<< HEAD
-    public class Inventory:List<Book>
-=======
     public class Inventory : List<Book>
->>>>>>> 72b01a69a834f3a8289030a9be6c7141ad47b9e9
     {
 
     }
