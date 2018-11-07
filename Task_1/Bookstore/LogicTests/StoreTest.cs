99c19c8147577df280f5dcd9a3c2d5359e00a950
Adam Dąbrowski <adam.dabrowski@htdevelopers.com>
2018-11-07 20:50:19 +0100

    Add Store test

--- Task_1/Bookstore/LogicTests/StoreTest.cs
+++ Task_1/Bookstore/LogicTests/StoreTest.cs
@@ -1,15 +1,18 @@
 using Xunit;
 using Logic;
+using Data;
 
 namespace LogicTests
 {
     public class StoreTest
     {
+        #region SuccessfulStocking
         [Theory]
-        [ClassData(typeof(StoreTestData))]
-        public void StockTest(Store store)
+        [MemberData(nameof(StoreTestData.GetStoresAndAffordableDeliveries), MemberType = typeof(StoreTestData))]
+        public void StockTest(Store store, Actor seller, float price, int count, ISBN isbn, Description description)
         {
-            Assert.True(true);
+            Assert.True(store.Stock(seller, price, count, isbn, description));
         }
+        #endregion
     }
 }
--- Task_1/Bookstore/LogicTests/StoreTestData.cs
+++ Task_1/Bookstore/LogicTests/StoreTestData.cs
@@ -1,20 +1,50 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using Logic;
+using System;
+using System.Collections;
+using Data;
 
 namespace LogicTests
 {
-    public class StoreTestData : IEnumerable<object[]>
+    public class StoreTestData
     {
-        StoreFactory storeFactory = new StoreFactory();
+        static Func<IEnumerator, object> advance = enumerator => { enumerator.MoveNext(); return enumerator.Current; };
 
-        public IEnumerator<object[]> GetEnumerator()
+        public static IEnumerable<Store> GetStores()
         {
-            yield return new object[] { storeFactory.CreateStore(.0f) };
-            yield return new object[] { storeFactory.CreateStore(1000f) };
-            yield return new object[] { storeFactory.CreateStore(1000000f) };
+            StoreFactory storeFactory = new StoreFactory();
+
+            yield return storeFactory.CreateStore(.0f);
+            yield return storeFactory.CreateStore(1000f);
+            yield return storeFactory.CreateStore(1000000f);
         }
 
-        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+        public static IEnumerable<Actor> GetActors()
+        {
+            yield return new Actor("John Smith");
+            yield return new Actor("Maria Skłodowska-Curie");
+            yield return new Actor("Los Nombres Españoles Son Demasiado Largos");
+        }
+
+        public static IEnumerable<Description> GetDescriptions()
+        {
+            yield return new Description("The Final Empire", "Brandon Sanderson");
+            yield return new Description("Ostatnie Życzenie", "Andrzej Sapkowski");
+            yield return new Description("Harry Potter and the Philosopher's Stone", "J.K. Rowling");
+        }
+
+        public static IEnumerable<object[]> GetStoresAndAffordableDeliveries()
+        {
+            using (IEnumerator<Store> stores = GetStores().GetEnumerator())
+            using (IEnumerator<Actor> actors = GetActors().GetEnumerator())
+            using (IEnumerator<Description> descriptions = GetDescriptions().GetEnumerator())
+            {
+                yield return new object[] { advance(stores), advance(actors), .0f, 1, new ISBN("1234567890123"), advance(descriptions) };
+
+                yield return new object[] { advance(stores), advance(actors), 100f, 10, new ISBN("1234567890123"), advance(descriptions) };
+
+                yield return new object[] { advance(stores), advance(actors), .01f, 10000, new ISBN("1234567890123"), advance(descriptions) };
+            }
+        }
     }
 }
